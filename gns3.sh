d4444f41d0ae1e89482102a52bb18187ac9e3f5f
Kyriakos <kyrgvlachos@gmail.com>
2023-03-09 09:35:09 +0200

    Update gns3.sh

--- gns3.sh
+++ gns3.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+ip=sudo ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
+echo $ip > test.txt
+gw=ip r | grep "default" | awk '{ print $3}'
+echo $gw >> test.txt
+ping -c $gw >> test.txt
 
-ping -c 5 8.8.8.8 > test.txt
-traceroute 8.8.8.8 > test.txt
+ping -c 5 8.8.8.8 >> test.txt
+traceroute 8.8.8.8 >> test.txt
 ls -la
 exit
