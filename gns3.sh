f6a48002ffe2024d5b46515fb51550d2bdfe6eae
Kyriakos <kyrgvlachos@gmail.com>
2023-03-09 23:47:15 +0200

    Update gns3.sh

--- gns3.sh
+++ gns3.sh
@@ -4,7 +4,7 @@ ip=$(sudo ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]
 gw=$(ip r | grep "default" | awk '{ print $3}')
 echo "your ip address: $ip and your gateway: $gw"
 ip2=$(echo $ip | cut -d '.' -f1-3)
-mitroo=$(echo "${mitroo//.}")
+mitroo=$(echo "${ip2//.}")
 file="$mitroo"".txt"
 
 echo -e $mitroo '\t' $ip '\t' $gw  > $file
