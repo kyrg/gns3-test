c79f1e24043df6ca4a8344c6987d24780815a6c0
Kyriakos <kyrgvlachos@gmail.com>
2023-03-09 13:01:02 +0200

    Update gns3.sh

--- gns3.sh
+++ gns3.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 ip=$(sudo ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 gw=$(ip r | grep "default" | awk '{ print $3}')
 echo "your ip address: $ip and your gateway: $gw"
