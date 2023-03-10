3e765150a56117462f07d86496b20f2f15898d39
Kyriakos <kyrgvlachos@gmail.com>
2023-03-10 08:58:34 +0200

    Update gns3.sh

--- gns3.sh
+++ gns3.sh
@@ -8,17 +8,34 @@ mitroo=$(echo "${ip2//.}")
 file="$mitroo"".txt"
 echo -e $mitroo '\t' $ip '\t' $gw  > $file
 
+echo "Checking Gateway"
+gw2="$ip2"".1"
+echo $gw2
+
+if [ $gw2 == $gw ]; then
+                message="GW_CORRECT"
+                echo $message
+                echo $message >> $file
+else
+                message="GW_FAILED"
+                echo $message
+                echo $message >> $file
+
+fi
+
+
+
 echo "begin ping to gateway"
 intertube=0
 while [ $intertube -ne 1 ]; do  
         line=$(ping -c 5 $gw  | tail -n 1)
         if [ $? -eq  0 ]; then
-                message="ping to GW SUCCESS"
+                message="ping to GW_SUCCESS"
                 echo $message
                 echo $message >> $file                
                 intertube=1;
         else 
-                message="ping to GW FAILED"
+                message="ping to GW_FAILED"
                 echo $message
                 echo $message >> $file        
 fi
@@ -29,12 +46,12 @@ while [ $intertube -ne 1 ]; do
         line=$(ping -c 5  8.8.8.8  | tail -n 1)
        # ping -c 5 8.8.8.8
         if [ $? -eq  0 ]; then
-                message="ping to 8.8.8.8 SUCCESS"
+                message="ping to 8.8.8.8_SUCCESS"
                 echo $message
                 echo $message >> $file
                 intertube=1;
        else
-                message="ping to 8.8.8.8 FAILED"
+                message="ping to 8.8.8.8_FAILED"
                 echo $message
                 echo $message >> $file
         fi
@@ -48,10 +65,10 @@ trace=$(echo $line | awk '{ print $2 }')
 
 if [ $trace == "8.8.8.8"  ]; then
 echo "you reach 8.8.8.8 in $hop hops"
-echo "traceroute SUCCESS " >> $file
+echo "traceroute_SUCCESS " >> $file
 else
 echo "destiantion 8.8.8.8 not reach"
-echo "traceroute FAILED" >> $file
+echo "traceroute_FAILED" >> $file
 fi
 
 echo "Checking Mikrotik Router"
@@ -67,11 +84,11 @@ echo $server
 line2=$(echo $line | grep "$server")
 echo $line2
 if [ -z "$line2" ]; then
-message="DHCP SERVER FAILED"
+message="DHCP_SERVER_FAILED"
 echo $message
 echo $message>> $file
 else
-message="DHCP SERVER SUCCESS"
+message="DHCP_SERVER_SUCCESS"
 echo $message
 echo $message>> $file
 
